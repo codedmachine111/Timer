fefb311e0ef0600b18a0141b930a5f69d2f35352
Akash <88738817+codedmachine111@users.noreply.github.com>
2021-09-25 21:34:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -14,10 +14,7 @@ function timer(){
 if(hour.value == 0 && minute.value == 0 && seconds.value == 0){
     hour.value = 0;
     minute.value = 0;
-    seconds.value = 0;
-        document.querySelector('#output').innerHTML = "Timeout!";
-
-    
+    seconds.value = 0;   
 stoptimer();
 
 }else if(seconds.value != 0){
@@ -39,14 +36,27 @@ clearInterval(starttimer);
 function stopit(){
 stoptimer();
 }
-
+function timeoutmsg(){
+    if(hour.value == 0 && minute.value == 0 && seconds.value == 0){
+        document.querySelector('#output').innerHTML = "Timeout!";
+    }
+}
+function alertmsg(){
+    if(hour.value == 0 && minute.value == 0 && seconds.value == 0){
+        alert("ERROR: Please set a timer");
+    }
+    stoptimer();
+}
 
 start.addEventListener('click', function(){
 
 function startinterval(){
     starttimer = setInterval(function(){
         timer();
+        timeoutmsg();
     }, 1000);
+    
+    alertmsg();
 }
 startinterval();
 })
@@ -63,5 +73,3 @@ reset.addEventListener('click', function(){
 stopp.addEventListener('click', function(){
 stopit();
 })
-
-
